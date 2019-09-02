fdd3d1e0317eb92a66ff1dd5ea6efa4136161f02
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-02 16:47:44 -0700

    [add] select and user input

--- checklist.py
+++ checklist.py
@@ -8,21 +8,17 @@ def create(item):
     # CREATE
     checklist.append(item)
 
-
 def read(index):
     # READ
     return checklist[index]
-
 
 def update(index, item):
     # UPDATE
     checklist[index] = item
 
-
 def destroy(index):
     # DESTROY
      checklist.pop(index)
-
 
 def list_all_items():
     # Code to list all items in list
@@ -33,22 +29,64 @@ def list_all_items():
 
 def mark_completed(index):
     # Add code here that marks an item as completed
+     update(index, "√" + checklist[index])
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+        return True
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        print(read(int(item_index)))
+        return True
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+        return True
+
+    elif function_code == "Q":
+        # This is where we want to stop our loop
+        return False
+
+    # Catch all
+    else:
+        print("Unknown Option")
+        return True
 
 def test():
     # Add your testing code here
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0,"purple socks")
-
-    destroy(1)
-
-    print(read(0))
-
     list_all_items()
+    mark_completed(0)
+
+    # Call your new function with the appropriate value
+    select("C")
+    #View results
+    list_all_items()
+    #Call function with new value
+    select("R")
+    #View results
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
 
 #Call functions
 test()
+
+running = True
+while running:
+    selection = user_input("Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
